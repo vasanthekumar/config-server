f523a454bd4ed2902b3d2fa2d06182fcb1bc7e14
Vasanth <vasanth.fine@yahoo.com>
2024-04-08 16:33:58 +0300

    Added code comments.

--- src/main/java/com/countrycloud/configserver/ConfigServerApplication.java
+++ src/main/java/com/countrycloud/configserver/ConfigServerApplication.java
@@ -4,10 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * Main class for the Config server application.
+ *
+ * @author kvasanthakumar
+ * @version 0.0.1
+ * Date: April 4,2024
+ */
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
 
+	/**
+	 * Main method to start the Config server.
+	 * @param args Command line arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
